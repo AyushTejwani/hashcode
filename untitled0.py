b3d4647ee4ea71175576f268bc1dbc03617b558c
AyushTejwani <tejwani.ayush@gmail.com>
2021-02-26 02:09:06 +0530

    input from file

--- untitled0.py
+++ untitled0.py
@@ -4,23 +4,28 @@ Created on Thu Feb 25 23:16:23 2021
 
 @author: tejwa
 """
+f=open("a.txt","r")
+content=f.read()
+cont=content.split()
 
-D=int(input())
-I=int(input())
-S=int(input())
-V=int(input())
-F=int(input())
+
+
+D=int(cont[0])
+I=int(cont[1])
+S=int(cont[2])
+V=int(cont[3])
+F=int(cont[4])
 streets=[]
 for i in range(0,S):
-    start=(int(input()))
-    end=(int(input()))
-    street=(input())
-    time=(int(input()))
+    start=cont[4+i*4+1]
+    end=cont[4+i*4+2]
+    street=cont[4+i*4+3]
+    time=cont[4+i*4+4]
     onestreet={
-        "start":start,
-        "end":end,
+        "start":int(start),
+        "end":int(end),
         "street":street,
-        "time":time
+        "time":int(time)
         }
     streets.append(onestreet)
    
@@ -29,13 +34,17 @@ for i in range(0,S):
 car_no_of_streets=[]
 car_street=[]
 cars=[]
+x=0
 for i in range(0,V):
-    x=(int(input()))
-   
-    
+   # print(4+S*4+i*(x+1)+1)
+  #  print(cont[4+S*4+i*(x+1)+1])
+    counter=4+S*4+i*(x+1)+1
+    x=int(cont[4+S*4+i*(x+1)+1])
     asd=[]
     for j in range(0,x):
-        asd.append(input())
+       # print(cont[4+S*4+i*(x+1)+2+j])
+       # print(4+S*4+i*(x+1)+2+j)
+        asd.append(cont[counter+1+j])
     one_car={
         'streets_visited':x,
         'names_of_streets':asd}
