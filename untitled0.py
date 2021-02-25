68ba14af6acbd9a16a78a467dd9d3eca5b484973
AyushTejwani <tejwani.ayush@gmail.com>
2021-02-26 01:14:11 +0530

    firsts

--- untitled0.py
+++ untitled0.py
@@ -10,35 +10,39 @@ I=int(input())
 S=int(input())
 V=int(input())
 F=int(input())
-start=[]
-end=[]
-street=[]
-time=[]
+streets=[]
 for i in range(0,S):
-    start.append(int(input()))
-    end.append((input()))
-    street.append(input())
-    
-    time.append(int(input()))
-
+    start=(int(input()))
+    end=(int(input()))
+    street=(input())
+    time=(int(input()))
+    onestreet={
+        "start":start,
+        "end":end,
+        "street":street,
+        "time":time
+        }
+    streets.append(onestreet)
+   
+for i in range(0,S):
+    print(streets[i])
 car_no_of_streets=[]
 car_street=[]
+cars=[]
 for i in range(0,V):
     x=(int(input()))
-    car_no_of_streets.append(x)
+   
+    
     asd=[]
     for j in range(0,x):
         asd.append(input())
-    car_street.append(asd)
+    one_car={
+        'streets_visited':x,
+        'names_of_streets':asd}
+    car_street.append(one_car)
+
+for i in range(0,V):
+    print(car_street[i])
         
-print(D)
-print(I)    
-print(S)
-print(V)
-print(F)
-for i in range(0,S):
-    print(start[i])
-    print(end[i])
-    print(street[i])
-    print(time[i])
+
  
